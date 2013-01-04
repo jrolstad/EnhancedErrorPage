664f57c3b6cd6b59e9b62ddc228d50bc47afb6a2
Josh Rolstad <jrolstad@gmail.com>
2013-01-04 12:59:27 -0800

    Initial commit

---
+++ MvcApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            using (var connection = new SqlConnection(@"Server=sea-2400-02\dev;Database=magnet_dev_joshr;Trusted_Connection=True;"))
+            {
+                try
+                {
+                    connection.Open();
+
+                    try
+                    {
+                        var data = connection.Query<DataEntity>("select 1 d as Value");
+                    }
+                    catch (Exception e)
+                    {
+                        var newException = new ApplicationException("oh oh, what happend?", e);
+                        throw newException;
+                    }
+                   
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/DataEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class DataEntity
+    {
+        public int Value { get; set; }
+    }
+}
