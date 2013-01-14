05ee74c84198ae33abe0326fcbacda19d7349b53
Josh Rolstad <jrolstad@gmail.com>
2013-01-14 12:58:02 -0800

    Removing old test project and integrating into main

--- CustomErrorPage.Mvc3/Global.asax.cs
+++ CustomErrorPage.Mvc3/Global.asax.cs
@@ -26,7 +26,7 @@ namespace CustomErrorPage.Mvc3
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Default", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
